24f4ad47fdb37d3c931ac0da6bd5d51addb2dea2
Krish123computerpro <100396122+Krish123computerpro@users.noreply.github.com>
2024-12-05 21:14:17 -0500

    Add files via upload

---
+++ Marvel Website.js
@@ -0,0 +1,39 @@
+console.log(
+    "This is my first time learning Js"
+);
+alert("Your Website crashed");
+let l_o_l = "Why aren't you doing something about it huh?";
+let lol = "YUR GAME CRASHED DO SOMETHING ABOUT IT CLICK CANCEL";
+let urdumb = "just exit I can't anymore";
+let stop_it = "stop looking at this just exit";
+let exit = "Alright you wanna press ok huh?";
+let gameon = "Knock Knock";
+let knock = "Who's there you say eh?";
+let riddle = "eggs";
+let answer = "Eggs who you say?";
+let leave = "EXIT OUT OF THE PAGE";
+let im_done = "im done. /ban user";
+alert(l_o_l);
+var status = confirm(lol);
+if (status == 'true') {
+    console.log("ok")
+    alert(urdumb);
+alert(stop_it);
+alert(exit);
+alert(gameon);
+alert(knock)
+alert(riddle);
+alert(answer);
+alert(leave);
+window.close();
+alert(im_done);
+function CLICK(){
+    alert(im_done);
+}
+function test(){
+    window.close();
+}
+}
+else {
+    alert("You chose the right decision buddy. :) I'm glad you listened.")
+}
